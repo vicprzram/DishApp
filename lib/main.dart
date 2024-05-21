d510ef4c6034267989f143c7d772f528865e857a
vicprzram <vicperamirez@gmail.com>
2024-05-21 13:56:47 +0200

    Nuevas recetas casi terminado 2

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class Main extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(useMaterial3: true),
-      home: AddrecipeWidget(),
+      home: LoginWidget(),
     );
   }
 }
